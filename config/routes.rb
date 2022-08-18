24c3010d85359b0ec7cda2b8efceca27e2344c4c
Gaby van den Born <gabyvdborn@gmail.com>
2022-08-19 00:11:50 +0800

    added controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'lists#index'
+  resources :lists, only: %i[index show new create] do
+    resources :bookmark, only: %i[index show new create]
+  end
+  resources :bookmark, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+25.times do
+  movie = Movie.new(
+    title:    Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    rating:  rand(0..10)
+  )
+  movie.save!
+end
